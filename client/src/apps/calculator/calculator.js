899a0fe3f744c7e67a0a19d71124c8423307dfd0
JJimenez <j.jimenez.994@hotmail.com>
2022-11-11 20:28:12 -0500

    some work

--- client/src/apps/calculator/calculator.js
+++ client/src/apps/calculator/calculator.js
@@ -6,7 +6,23 @@ const Calculator = () => {
       <div>
         <h2>Calculator</h2>
         <div className="calculator">
-          <div className="output">0</div>
+          <div className="calc-numbers">0</div>
+          <div className="div2">1</div>
+          <div className="div3">2</div>
+          <div className="div4">3</div>
+          <div className="div5">4</div>
+          <div className="div6">5</div>
+          <div className="div7">6</div>
+          <div className="div8">7</div>
+          <div className="div9">8</div>
+          <div className="div10">9</div>
+          <div className="div11">10</div>
+          <div className="div12">11</div>
+          <div className="div13">12</div>
+          <div className="div14">13</div>
+          <div className="div15">14</div>
+          <div className="div16">15</div>
+
         </div>
       </div>
     </div>
