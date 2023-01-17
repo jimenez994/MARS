8981e84de89db10159cb9dcd33842bd48cea3e20
JJimenez <j.jimenez.994@hotmail.com>
2023-01-16 19:31:47 -0500

    _

--- client/src/apps/calculator/calculator.js
+++ client/src/apps/calculator/calculator.js
@@ -7,21 +7,21 @@ const Calculator = () => {
         <h2>Calculator</h2>
         <div className="calculator">
           <div className="calc-numbers">0</div>
-          <div className="div2">1</div>
-          <div className="div3">2</div>
-          <div className="div4">3</div>
-          <div className="div5">4</div>
-          <div className="div6">5</div>
-          <div className="div7">6</div>
-          <div className="div8">7</div>
-          <div className="div9">8</div>
-          <div className="div10">9</div>
-          <div className="div11">10</div>
-          <div className="div12">11</div>
-          <div className="div13">12</div>
-          <div className="div14">13</div>
-          <div className="div15">14</div>
-          <div className="div16">15</div>
+          <div className="divAC">AC</div>
+          <div className="divPN">+/-</div>
+          <div className="divPercent">%</div>
+          <div className="divDivide">/</div>
+          <div className="div7">7</div>
+          <div className="div8">8</div>
+          <div className="div9">9</div>
+          <div className="divX">X</div>
+          <div className="div4">4</div>
+          <div className="div5">5</div>
+          <div className="div6">6</div>
+          <div className="divMenus">-</div>
+          <div className="div1">1</div>
+          <div className="div2">2</div>
+          <div className="div3">3</div>
 
         </div>
       </div>
